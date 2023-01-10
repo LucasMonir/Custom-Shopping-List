7312e92fe7d251a32d797e59da64680fbf0b11e9
LucasMonir <lucasmonir@hotmail.com>
2023-01-10 20:26:05 -0300

    Added a sliverappbar, fixed list size

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,49 +1,50 @@
+import 'dart:html';
+
 import 'package:custom_wish_list/widgets/tile_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
   const HomePage({super.key, required this.title});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   List<String> list = List.generate(80, (index) => 'Item #$index');
+  final Color backgroundButtonColor = Colors.white;
+  final Color hoverButtonColor = Colors.grey;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Title(
-          color: Colors.black,
-          child: const Center(
-            child: Text('Shopping list'),
-          ),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          title: Text(widget.title),
+          floating: true,
+          flexibleSpace: Image.asset('aaaaa'),
+          expandedHeight: 50,
         ),
-      ),
-      body: Stack(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-          ),
-          ListView.builder(
-            cacheExtent: 25,
-            itemBuilder: (context, index) => TileWidget(
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: list.length,
+            (context, index) => TileWidget(
               title: list[index],
               index: index,
               price: index.toString(),
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+      ],),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(197, 172, 97, 252),
-        hoverColor: const Color.fromARGB(197, 95, 53, 139),
+        backgroundColor: backgroundButtonColor,
+        hoverColor: hoverButtonColor,
+        elevation: 10,
         onPressed: () {
           setState(() {});
         },
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add, color: Colors.black),
       ),
     );
   }
