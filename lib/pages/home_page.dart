310ab5f52d41ec33ee468c33702b29e2205d8a15
LucasMonir <lucasmonir@hotmail.com>
2023-01-11 21:01:38 -0300

    Added bottom navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
-import 'dart:html';
-
+import 'dart:io';
 import 'package:custom_wish_list/widgets/tile_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -12,39 +11,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> list = List.generate(80, (index) => 'Item #$index');
+  List<String> list = List.generate(20, (index) => 'Item #$index');
   final Color backgroundButtonColor = Colors.white;
   final Color hoverButtonColor = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(slivers: [
-        SliverAppBar(
-          title: Text(widget.title),
-          floating: true,
-          flexibleSpace: Image.asset('aaaaa'),
-          expandedHeight: 50,
-        ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: list.length,
-            (context, index) => TileWidget(
-              title: list[index],
-              index: index,
-              price: index.toString(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Center(
+              child: Text(
+                widget.title,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            floating: true,
+            toolbarHeight: 100,
+            flexibleSpace: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset(
+                    'assets/header.jpg',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ],),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: backgroundButtonColor,
-        hoverColor: hoverButtonColor,
-        elevation: 10,
-        onPressed: () {
-          setState(() {});
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: list.length,
+              (context, index) => TileWidget(
+                title: list[index],
+                index: index,
+                price: index.toString(),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add new item',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.close),
+            label: 'Add new item',
+          ),
+        ],
+        onTap: (int index) {
+          if (index == 0) {
+            print('todo: add item to list');
+          }
+          if (index == 1) {
+            exit(0);
+          }
         },
-        child: const Icon(Icons.add, color: Colors.black),
       ),
     );
   }
--- lib/widgets/dark_mode_switch.dart
+++ lib/widgets/dark_mode_switch.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'app_widget.dart';
 
 class CustomSwitcher extends StatelessWidget {
