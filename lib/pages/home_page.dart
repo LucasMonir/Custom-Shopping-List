fa554bf4fc76584e04d20d8f96e8c9c7504611a8
LucasMonir <lucasmonir@hotmail.com>
2023-01-18 22:38:27 -0300

    Added list item removal, removal icon on dismiss and replaced bottom buttons for an elevated button, todo: add logic

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,11 @@
 import 'package:custom_wish_list/widgets/appbar_widget.dart';
-import 'package:custom_wish_list/widgets/bottom_navigation_widget.dart';
 import 'package:custom_wish_list/widgets/tile_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  late List<String> list;
-  late String title;
-  late Function callback;
+  final List<String> list = List.generate(11, (index) => '$index');
+  final String title;
 
-  HomePage.callback({super.key, required this.callback});
   HomePage({super.key, required this.title});
 
   @override
@@ -18,7 +15,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    widget.list = List.generate(20, (index) => 'Item #$index');
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
@@ -27,32 +25,64 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
-        bottomNavigationBar: const BottomNavigationWidget(),
-        body: CustomScrollView(
-          slivers: <Widget>[
-            AppbarWidget(title: widget.title),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                childCount: widget.list.length,
-                (context, index) => Dismissible(
-                  key: UniqueKey(),
-                  child: TileWidget(
-                    title: widget.list[index],
-                    index: index,
-                    price: index.toString(),
+        persistentFooterButtons: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(screenWidth, double.infinity),
+              backgroundColor: const Color.fromARGB(255, 48, 75, 209),
+              elevation: 50,
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                shadows: [
+                  Shadow(
+                    blurRadius: 2,
+                    color: Colors.black,
+                    offset: Offset(1, 2),
                   ),
-                  onDismissed: (direction) {
-                    setState(() {
-                      widget.list.removeAt(index);
-                    });
-                  },
-                ),
+                ],
               ),
             ),
-          ],
+            onPressed: () {},
+            child: const Text('Add Item!'),
+          )
+        ],
+        backgroundColor: Colors.transparent,
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) =>
+              [AppbarWidget(title: widget.title)],
+          body: ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: widget.list.length,
+            itemBuilder: (context, index) => Dismissible(
+              key: UniqueKey(),
+              direction: DismissDirection.endToStart,
+              onDismissed: (DismissDirection direction) {
+                setState(() {
+                  widget.list.removeAt(index);
+                });
+              },
+              background: Container(),
+              secondaryBackground: deleteIcon,
+              child: TileWidget(
+                title: widget.list[index],
+                index: index,
+                price: index.toString(),
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
+
+  final deleteIcon = Container(
+    color: Colors.redAccent,
+    alignment: Alignment.centerRight,
+    child: const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Icon(Icons.delete_forever),
+    ),
+  );
 }
